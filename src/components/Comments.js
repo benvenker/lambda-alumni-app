dbcf738069b5cc5123445dee7507f03f9addbf41
Ben Venker <bvemails@gmail.com>
2020-05-14 16:41:16 -0500

    add loading spinner to comments componenent

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 import "./Comments.css";
 import Comment from "./Comment";
 
 const Comments = (props) => {
   const { postId } = props;
-
+  const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/comments/${postId}`)
-      .then((response) => setComments(response.data));
-  });
+    setLoading(true);
+    axios.get(`http://localhost:5000/comments/${postId}`).then((response) => {
+      setComments(response.data);
+      setLoading(false);
+      console.log({ comments });
+    });
+  }, []);
 
   return (
     <div className="comment">
-      {comments.map((comment) => {
-        return <Comment comment={comment} />;
-      })}
+      {loading ? (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000}
+          margin="auto"
+        />
+      ) : (
+        comments.map((comment) => {
+          return <Comment comment={comment} />;
+        })
+      )}
     </div>
   );
 };
