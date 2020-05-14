e59660ea0f59efdf4b9d2c055660394afa4c803b
Ben Venker <bvemails@gmail.com>
2020-05-13 21:27:07 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+import { css } from "emotion";
 import "./App.css";
+
+const styles = css({
+  app: {
+    fontFamily: "Fira Code",
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Route path="/">
-        <Home />
+      <Route exact path="/">
+        <Home className={styles.app} />
+      </Route>
+      <Route path="/post/:id">
+        <Post className={styles.app} />
+      </Route>
+      <Route path="/posts">
+        <Posts className={styles.app} />
       </Route>
     </div>
   );
