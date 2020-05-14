c596c46c57131ffe904eeb61130ff8c87e623017
Ben Venker <bvemails@gmail.com>
2020-05-13 21:28:52 -0500

    create Post component

---
+++ src/components/Post.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Post.css";
+import ThumbIcon from "./ThumbIcon";
+import { css } from "emotion";
+
+const Post = (props) => {
+  const { post } = props;
+
+  const styles = css({
+    post: {
+      border: "1px solid red",
+      display: "flex",
+    },
+  });
+
+  return (
+    <div key={post.id} className="post-container">
+      <div className="row">
+        <div className="vote">
+          <ThumbIcon />
+        </div>
+        <div className="post-title">{post.title}</div>
+        <div className="post-url">({post.url.slice(8)})</div>
+      </div>
+      <div className="row">
+        by {post.user} | {post.votes} votes | {post.comments} comments
+      </div>
+    </div>
+  );
+};
+
+export default Post;
