4960c77d442b9c5bb85531c441280a301042c906
Ben Venker <bvemails@gmail.com>
2020-05-19 14:10:28 -0500

    well, we're doing something with mobile CSS

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaAlignRight } from "react-icons/fa";
 import Logo from "./Logo";
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-  console.log("searchbar props:", props);
+  const [toggled, setToggled] = useState(false);
   const { auth } = props;
+
   return (
-    <div className="search-bar">
-      <Logo dimensions={45} />
+    <div className="nav-bar">
+      <button className="hamburger-menu" onClick={() => setToggled(!toggled)}>
+        <FaAlignRight />
+      </button>
+
+      <ul className={toggled ? "nav-links show-nav" : "nav-links"}>
+        <li href="#">
+          {" "}
+          <Logo dimensions={30} />
+        </li>
+        <li href="#">
+          <input type="text" placeholder="Search all posts..." />
+        </li>
+        <li href="#">
+          {" "}
+          <button
+            className="logout-button"
+            onClick={auth.isAuthenticated() ? auth.logout : auth.login}
+          >
+            {auth.isAuthenticated() ? "Log out" : "Log In"}
+          </button>
+        </li>
+      </ul>
+
+      {/* <Logo dimensions={45} />
 
       <input type="text" placeholder="Search all posts..." />
       <span>
@@ -17,7 +42,7 @@ const SearchBar = (props) => {
         >
           {auth.isAuthenticated() ? "Log out" : "Log In"}
         </button>
-      </span>
+      </span> */}
     </div>
   );
 };
