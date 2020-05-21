0630f4367510357271006df13cd31cc21cb8e10a
Ben Venker <bvemails@gmail.com>
2020-05-21 17:52:00 -0500

    store profile in posts state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import Post from "./Post";
 import ButtonGoogle from "./ButtonGoogle";
@@ -9,12 +9,42 @@ import SearchBar from "./SearchBar";
 
 const Posts = (props) => {
   const { auth } = props;
+  const [profile, setProfile] = useState({ email: "" });
+  const [error, setError] = useState("");
+
   const history = useHistory();
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:5000/posts`).then((res) => setPosts(res.data));
   }, []);
+
+  // Get the user profile and post new users to the API
+  useEffect(() => {
+    const loadUserProfile = () => {
+      auth.getProfile((profile, err) => {
+        setProfile(auth.userProfile);
+        console.log("Profile: ", auth.userProfile);
+        setError(err);
+      });
+    };
+
+    loadUserProfile();
+
+    // const body = { username: auth.userProfile.email };
+
+    // if (profile) {
+    //   if (profile.username.length > 1) {
+    //     console.log(
+    //       axios
+    //         .post(`http://localhost:5000/add-user`, body)
+    //         .then((respone) => console.log(respone))
+    //     );
+    //   }
+    // }
+
+    console.log("Auth profile from posts: ", profile);
+  }, [profile, auth]);
 
   return (
     <>
