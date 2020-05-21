b82954f02724b471c44f893eb95ec2d913780d6c
Ben Venker <bvemails@gmail.com>
2020-05-21 17:50:31 -0500

    remove bogus css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
-import { FaAlignRight } from "react-icons/fa";
+import React from "react";
 import Logo from "./Logo";
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-  const [toggled, setToggled] = useState(false);
   const { auth } = props;
 
-  const navItems = [
-    { link: "/", content: <Logo dimensions={30} /> },
-    { item: "", content: <input type="text" /> },
-    {
-      item: "",
-      content: (
-        <button
-          className="logout-button"
-          onClick={auth.isAuthenticated() ? auth.logout : auth.login}
-        >
-          {auth.isAuthenticated() ? "Log out" : "Log In"}
-        </button>
-      ),
-    },
-  ];
   return (
     <div className="nav-bar">
       <Logo dimensions={45} />
