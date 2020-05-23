5bc4278bdbc308a4bdc148173da9c3417d947531
Ben Venker <bvemails@gmail.com>
2020-05-23 05:58:28 -0500

    display usernames in comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,15 +4,15 @@ import "./Comment.css";
 const Comment = (props) => {
   const { comment } = props;
   return (
-    <>
+    <div className="comment" key={comment.id}>
       <div className="comment-info">
-        {comment.user_id} {comment.created_date}
+        {comment.username} at {comment.user_id} {comment.created_date}
       </div>
       <div className="comment-body">
         <p>{comment.body}</p>
       </div>
       {/* <button>REPLY</button> */}
-    </>
+    </div>
   );
 };
 
