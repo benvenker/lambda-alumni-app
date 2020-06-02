237b25a8357f650093db7a8f0f1c1c2e113e6f6b
Ben Venker <bvemails@gmai.com>
2020-06-02 11:43:35 -0500

    refactor comments with tailwind

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,11 +4,11 @@ import "./Comment.css";
 const Comment = (props) => {
   const { comment } = props;
   return (
-    <div className="comment" key={comment.id}>
+    <div className="comment font-bold text-xs my-8" key={comment.id}>
       <div className="comment-info">
         {comment.username} on {comment.created_date}
       </div>
-      <div className="comment-body">
+      <div className="comment-body p-8">
         <p>{comment.body}</p>
       </div>
       {/* <button>REPLY</button> */}
