cff5755c0c2d9e61171fa1a9046f01c4e20f368f
Ben Venker <bvemails@gmai.com>
2020-06-02 14:58:40 -0500

    refactor SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,12 +6,16 @@ const SearchBar = (props) => {
   const { auth } = props;
 
   return (
-    <div className="nav-bar">
+    <div className="nav-bar bg-gray-700 w-full p-3 pt-5 flex text-white">
       <Logo dimensions={45} />
-      <input type="text" placeholder="Search all posts..." />
+      <input
+        className="bg-gray-800 w-3/4 h-8 border-none rounded-full pl-5 focus:outline-none"
+        type="text"
+        placeholder="Search all posts..."
+      />
       <span>
         <button
-          className="logout-button"
+          className="logout-button text-white bg-gray-800 text-xs border-none ml-2 px-3 py-1 rounded-md"
           onClick={auth.isAuthenticated() ? auth.logout : auth.login}
         >
           {auth.isAuthenticated() ? "Log out" : "Log In"}
