73a8e01382bfb36153e7b649276d2e6400c23b7d
Ben Venker <bvemails@gmai.com>
2020-06-03 15:34:18 -0500

    if user is authenticated push them to posts page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Logo from "./Logo";
-import { css } from "emotion";
+import { useHistory } from "react-router-dom";
 import ButtonGoogle from "./ButtonGoogle";
-import LoginForm from "./LoginForm";
 
 const Home = (props) => {
   const { auth } = props;
+  const history = useHistory();
+
   return (
-    <div className="text-center flex-col justify-center">
-      <h1 className="text-5xl w-64 mx-auto my-3">Lambda Alumni Network</h1>
-      <Logo dimensions={180} />
-      <ButtonGoogle auth={auth} />
-      {/* <LoginForm /> */}
+    <div auth={auth} className="flex flex-col justify-center items-center">
+      {!auth.isAuthenticated() ? (
+        <div className="text-center flex flex-col justify-center items-center w-2/4 m-auto mt-48 bg-white rounded-md">
+          <h1 className="text-5xl w-64 mx-auto my-3">Lambda Alumni Network</h1>
+          <div>
+            <Logo dimensions={180} />
+          </div>
+          <ButtonGoogle auth={auth} />
+          {/* <LoginForm /> */}
+        </div>
+      ) : (
+        history.push("/posts")
+      )}
     </div>
   );
 };
