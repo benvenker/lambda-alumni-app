f2628b11eb01da1a8dc2a6d25f7f74b247e276e7
Ben Venker <bvemails@gmai.com>
2020-06-07 20:39:46 -0500

    change url length check

--- src/components/Post.js
+++ src/components/Post.js
@@ -62,7 +62,7 @@ const Post = (props) => {
           <div className="flex flex-row">
             {history.location.pathname === "/posts" ? (
               <div className="hover:underline text-gray-700 px-3 text-md font-semibold my-0">
-                {post.url.length > 1 ? (
+                {post.url.length > 0 ? (
                   <a href={`${post.url}`}>{post.title}</a>
                 ) : (
                   <Link to={`post/${post.id}`}>{post.title}</Link>
